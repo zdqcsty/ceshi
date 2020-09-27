89b5422473020686d5654ed5938e9dd8364e3040
zengguanghao <zeng92922@163.com>
2020-09-27 20:14:11 +0800

    20200927

---
+++ src/main/java/com/example/ceshi/sys/ToiletRace.java
@@ -0,0 +1,50 @@
+package com.example.ceshi.sys;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+class Employee implements Runnable {
+    private String id;
+    private Semaphore semaphore;
+    private static Random rand= new Random(47);
+
+    public Employee(String id, Semaphore semaphore) {
+        this.id = id;
+        this.semaphore = semaphore;
+    }
+
+    public void run() {
+        try {
+            semaphore.acquire();
+            System.out.println(this.id + "is using the toilet");
+            TimeUnit.MILLISECONDS.sleep(rand.nextInt(2000));
+            semaphore.release();
+            System.out.println(this.id + "is leaving");
+        } catch (InterruptedException e) {
+        }
+    }
+}
+
+public class ToiletRace {
+    private static final int THREAD_COUNT = 30;
+
+    private static ExecutorService threadPool = Executors
+            .newFixedThreadPool(THREAD_COUNT);
+
+    private static Semaphore s = new Semaphore(2);
+
+    public static void main(String[] args) {
+
+        Semaphore demo = new Semaphore(2);
+        demo.tryAcquire()
+
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            threadPool.execute(new Employee(String.valueOf(i), s));
+        }
+
+        threadPool.shutdown();
+    }
+}
