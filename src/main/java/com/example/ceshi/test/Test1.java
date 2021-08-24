9d40f2cc754953e6374090942d6ea370bd8a74ff
zengguanghao <zengguanghao@bonc.com.cn>
2021-08-24 11:28:23 +0800

    整理5

---
+++ src/main/java/com/example/ceshi/test/Test1.java
@@ -0,0 +1,30 @@
+package com.example.ceshi.test;
+
+import java.io.ObjectOutputStream;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        Bbb bbb=new Bbb();
+    }
+
+
+
+
+
+}
+
+
+class Bbb{
+
+    String aaa;
+
+    public String getAaa() {
+        return aaa;
+    }
+
+    public void setAaa(String aaa) {
+        this.aaa = aaa;
+    }
+}
